f3b69596c00e11b2a4b2a27f7be3bae25966fa2a
Ashuhauhan <ashuc306@gmail.com>
2022-12-14 16:41:45 +0530

    totalwage for particular numbers of hours(or days)

--- src/UC6_EmployeeAttendence.java
+++ src/UC6_EmployeeAttendence.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class UC5_EmployeeAttendence {
+public class UC6_EmployeeAttendence {
     public static void main(String[] args) {
         int r;
         Random ran = new Random();
@@ -18,11 +18,14 @@ public class UC5_EmployeeAttendence {
             int fulltimehours = 8;
             int parttimehours = 4;
             int wageperday= wageperhour * fulltimehours;
+            //Calculation of wage per day of employee.
             System.out.println("daily wage of fulltime working Employee is " +wageperday);
-            int monthlyworkingdays = 20 ;
-            int monthlywage;
-            monthlywage= monthlyworkingdays * wageperday ;
-            System.out.println("monthlywage of Employee is  " + monthlywage);
-        }}
-
+            //Calculation of wages till a condition of total working hours(or days) is reached for a month.
+            //here assume 100hours(or 20 days).
+            int monthlyworkingdays = 20;
+            int totalwage = wageperday * monthlyworkingdays;
+            System.out.println("totalwage of Employee for 20days is      " + totalwage);
+        }
+    }
 }
+
